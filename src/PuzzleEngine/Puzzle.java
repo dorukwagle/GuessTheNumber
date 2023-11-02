9eb04ea2974474be3ca19cb8257bc10a98e5e455
dorukwagle <dorukwagle@gmail.com>
2023-11-02 20:33:07 +0545

    implement puzzle parser

--- src/PuzzleEngine/Puzzle.java
+++ src/PuzzleEngine/Puzzle.java
@@ -1,4 +1,22 @@
 package PuzzleEngine;
 
+import java.util.List;
+
 public class Puzzle {
+
+    private final List<List<String>> puzzle;
+    private final int solution;
+
+    public Puzzle(List<List<String>> puzzle, int hiddenNumber) {
+        this.puzzle = puzzle;
+        this.solution = hiddenNumber;
+    }
+
+    public List<List<String>> getPuzzle() {
+        return this.puzzle;
+    }
+
+    public int getSolution() {
+        return this.solution;
+    }
 }
--- src/PuzzleEngine/PuzzleParser.java
+++ src/PuzzleEngine/PuzzleParser.java
@@ -1,7 +1,9 @@
 package PuzzleEngine;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PuzzleParser {
     private List<List<Integer>> puzzleMatrix;
@@ -10,7 +12,7 @@ public class PuzzleParser {
     private int rowCount;
     private int colCount;
 
-    private List<List<String>> puzzle;
+    private List<List<String>> puzzle = new ArrayList<>();
 
     public PuzzleParser(List<List<Integer>> puzzleMatrix, List<List<Operator>> operatorList, int hiddenNumber) {
         this.puzzleMatrix = puzzleMatrix;
@@ -25,18 +27,70 @@ public class PuzzleParser {
         var rowSum = getRowsSum();
         var colSum = getColumnsSum();
 
+        for (var i = 0; i < rowCount; ++i) {
+            var row = puzzleMatrix.get(i);
+            var rowPuzzle = new ArrayList<String>();
 
-        return null;
+            row.forEach(num -> rowPuzzle.add(hideNumber(num)));
+
+            rowPuzzle.add("=");
+            rowPuzzle.add(String.valueOf(rowSum[i]));
+
+            puzzle.add(rowPuzzle);
+        }
+
+        // add equals sign at second last row
+        var equalsRow = new ArrayList<String>();
+        for (var i = 0; i < colCount; ++i)
+            equalsRow.add("=");
+
+        // add colSum as the last row
+        var colSumRow = new ArrayList<String>();
+        Arrays.stream(colSum).forEach(num -> colSumRow.add(String.valueOf(num)));
+
+        puzzle.add(equalsRow);
+        puzzle.add(colSumRow);
+
+        return new Puzzle(puzzle, hiddenNumber);
+    }
+
+    private String hideNumber(int number) {
+        return String.valueOf(number).replaceAll(String.valueOf(hiddenNumber), "T");
     }
 
     private int[] getRowsSum() {
         var row = new int[rowCount];
+
+        for (var i = 0; i < rowCount; ++i) {
+            var list = puzzleMatrix.get(i);
+            var operList = operatorList.get(i);
+            var rowSum = 0;
+            for (var j = 0; j < colCount; ++i) {
+                var num = list.get(j);
+                var numSign = operList.get(j);
+                rowSum = numSign == Operator.Add ? rowSum + num : rowSum - num;
+            }
+            row[i] = rowSum;
+        }
 
         return row;
     }
 
     private int[] getColumnsSum() {
         var column = new int[colCount];
+
+        for (var i = 0; i < colCount; ++i) {
+            var colSum = 0;
+            for (var j = 0; j < rowCount; ++i) {
+                var list = puzzleMatrix.get(j);
+                var operList = operatorList.get(j);
+
+                var num = list.get(i);
+                var numSign = operList.get(i);
+                colSum = numSign == Operator.Add ? colSum + num : colSum - num;
+            }
+            column[i] = colSum;
+        }
 
         return column;
     }
